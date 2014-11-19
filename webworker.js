bafdaf5e2fafe6b13bab1cb10376a5d1e9855845
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-11-20 01:59:33 +0200

    Update webworker.js

--- webworker.js
+++ webworker.js
@@ -6,7 +6,7 @@ function multiplesOfSeven() {
         if(i%7==0){
             postMessage(i);   
         }
-         setTimeout("multiplesOfSeven()",500);
+         setTimeout("multiplesOfSeven()",1000);
     }
 }
 
